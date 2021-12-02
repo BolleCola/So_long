13a67dc9764e9d39a6c46bfc363472efed74985d
BolleCola <tpaquier@student.42lausanne.ch>
2021-12-02 15:03:10 +0100

    jeudi

--- so_long.h
+++ so_long.h
@@ -4,21 +4,17 @@
 #include "./mlx/mlx.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 // gcc -Lmlx -lmlx -framework OpenGL -framework AppKit game.a
 
 typedef struct s_data {
-    void *img;
-    char *addr;
-    int bits_per_pixel;
-    int line_length;
-    int endian;
-}   t_data;
+    void 	*img;
+    char 	*addr;
+}    t_data;
 
 typedef struct s_vars{
     void *mlx;
     void *win;
 }   t_vars;
-
-void    my_mlx_pixel_put(t_data *data, int x, int y, int color);
 
 #endif
--- test.c
+++ test.c
@@ -1,83 +1,82 @@
 #include "so_long.h"
 
-void    my_mlx_pixel_put(t_data *data, int x, int y, int color)
+int close_test(int keycode, t_vars *vars)
 {
-	char *dst;
-
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-	*(unsigned int*)dst = color;
-}
-
-int close(int keycode, t_vars *vars, t_data img)
-{
-	static int i;
-	static int j;
-	
-	if (!j)
-		j = 0;
-	if (!i)
-		i = 0;
+	static int i = 32;
+	static int j = 32;
+	t_data	img2;
+	int	hauteur = 16;
+	int largeur = 16;
+	char *pers = "./pack/xpm/perso_f.xpm";
+	char *grass = "./pack/xpm/Grass.xpm";
+	char *persb = "./pack/xpm/perso_b.xpm";
+	char *persg = "./pack/xpm/perso_g.xpm";
+	char *persd = "./pack/xpm/perso_d.xpm";
+	if (i == 272)
+		i-=16;
+	if (i == 0)
+		i+=16;
+	if (j == 128)
+		j-=16;
+	if (j == 0)
+		j+=16;
 	if (keycode == 53)
 	{
 		mlx_destroy_window(vars->mlx, vars->win);
 		exit (1);
 	}
-	if (keycode == 1)
+	if (keycode == 1 && j < 128 && j > 16)
 	{
-		img.img = mlx_new_image(vars->mlx, 640, 480);
-		img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-		my_mlx_pixel_put(&img, j, i++, 0xFFC0CB);
-		mlx_put_image_to_window(vars->mlx, vars->win, img.img, 0, 0);
+		img2.img = mlx_xpm_file_to_image(vars->mlx, pers, &hauteur, &largeur);
+		mlx_put_image_to_window(vars->mlx, vars->win, img2.img, i, j+=16);
+		img2.img = mlx_xpm_file_to_image(vars->mlx, grass, &hauteur, &largeur);
+		mlx_put_image_to_window(vars->mlx, vars->win, img2.img, i, j-16);
 	}
-	if (keycode == 13)
+	if (keycode == 13 && j < 128 && j > 16)
 	{
-		img.img = mlx_new_image(vars->mlx, 640, 480);
-		img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-		my_mlx_pixel_put(&img, j, i--, 0xFFC0CB);
-		mlx_put_image_to_window(vars->mlx, vars->win, img.img, 0, 0);
+		img2.img = mlx_xpm_file_to_image(vars->mlx, persb, &hauteur, &largeur);
+		mlx_put_image_to_window(vars->mlx, vars->win, img2.img, i, j-=16);
+		img2.img = mlx_xpm_file_to_image(vars->mlx, grass, &hauteur, &largeur);
+		mlx_put_image_to_window(vars->mlx, vars->win, img2.img, i, j+16);
 	}
-	if (keycode == 2)
+	if (keycode == 2 && i < 288 && i > 16)
 	{
-		img.img = mlx_new_image(vars->mlx, 640, 480);
-		img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-		my_mlx_pixel_put(&img, j++, i, 0xFFC0CB);
-		mlx_put_image_to_window(vars->mlx, vars->win, img.img, 0, 0);
+		img2.img = mlx_xpm_file_to_image(vars->mlx, persd, &hauteur, &largeur);
+		mlx_put_image_to_window(vars->mlx, vars->win, img2.img, i+=16, j);
+		img2.img = mlx_xpm_file_to_image(vars->mlx, grass, &hauteur, &largeur);
+		mlx_put_image_to_window(vars->mlx, vars->win, img2.img, i-16, j);
 	}
-	if (keycode == 0)
+	if (keycode == 0 && i < 288 && i > 16)
 	{
-		img.img = mlx_new_image(vars->mlx, 640, 480);
-		img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-		my_mlx_pixel_put(&img, j--, i, 0xFFFFFF);
-		mlx_put_image_to_window(vars->mlx, vars->win, img.img, 0, 0);
+		img2.img = mlx_xpm_file_to_image(vars->mlx, persg, &hauteur, &largeur);
+		mlx_put_image_to_window(vars->mlx, vars->win, img2.img, i-=16, j);
+		img2.img = mlx_xpm_file_to_image(vars->mlx, grass, &hauteur, &largeur);
+		mlx_put_image_to_window(vars->mlx, vars->win, img2.img, i+16, j);
 	}
-	return (0);
-}
-
-int	render_next_frame(t_vars *vars)
-{
-	return (mlx_hook(vars->win, 2, 1L>>0, close, vars));
+	return (-1);
 }
 
 int main(void)
 {
     t_vars vars;
 	t_data img;
-		
 	char *grass = "./pack/xpm/Grass.xpm";
 	char *rock = "./pack/xpm/Rock.xpm";
 	char *chest = "./pack/xpm/chest_c.xpm";
+	char *sign = "./pack/xpm/Sign.xpm";
 	char *pers = "./pack/xpm/perso_f.xpm";
 	int	hauteur = 16;
 	int largeur = 16;
 	int	i;
 	int j;
+	int x;
+	x = -1;
 	j = 0;
 	i = 0;
 
 	vars.mlx = mlx_init();
-	vars.win = mlx_new_window(vars.mlx, 320, 160, "Bolle's Dungeon");
+	vars.win = mlx_new_window(vars.mlx, 1920, 1080, "Bolle's Dungeon");
 	img.img = mlx_xpm_file_to_image(vars.mlx, grass, &hauteur, &largeur);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
 	while (i <= 480)
 	{
 		if (i >= 480 && j < 640)
@@ -115,39 +114,10 @@ int main(void)
 	}
 	img.img = mlx_xpm_file_to_image(vars.mlx, chest, &hauteur, &largeur);
 	mlx_put_image_to_window(vars.mlx, vars.win, img.img, 32, 16);
-	mlx_loop_hook(vars.mlx, render_next_frame, &vars);
+	img.img = mlx_xpm_file_to_image(vars.mlx, sign, &hauteur, &largeur);
+	mlx_put_image_to_window(vars.mlx, vars.win, img.img, 64, 64);
+	img.img = mlx_xpm_file_to_image(vars.mlx, pers, &hauteur, &largeur);
+	mlx_put_image_to_window(vars.mlx, vars.win, img.img, 32, 32);
+	mlx_key_hook(vars.win, close_test, &vars);
 	mlx_loop(vars.mlx);
-	
-	/*vars.mlx = mlx_init();
-	vars.win = mlx_new_window(vars.mlx, 640, 480, "Bolle's Dungeon");
-	mlx_loop_hook(vars.mlx, render_next_frame, &vars);
-	mlx_loop(vars.mlx);*/
-
-	/*void    *mlx_win;
-   	vars.mlx = mlx_init();
-	vars.win = mlx_new_window(vars.mlx, 640, 480, "Bolle's Dungeon !");
-	mlx_hook(vars.win, 2, 1L<<0, close, &vars);
-	mlx_key_hook(vars.win, key_hook, &vars);
-	mlx_loop(vars.mlx);
-
-    mlx = mlx_init();
-    mlx_win = mlx_new_window(mlx, 1080, 720, "Bolle's Dungeon");
-    img.img = mlx_new_image(mlx, 1080, 720);
-    img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-
-	int i = 360;
-	int	j = 360;
-	while (j != 460)
-    	my_mlx_pixel_put(&img, 530, j++, 0xFF67C5);
-	while (i != 460)
-		my_mlx_pixel_put(&img, 532, i++, 0x6A329F);
-	j = 360;
-	while (j != 460)
-		my_mlx_pixel_put(&img, 534, j++, 0xFFC0CB);
-	i = 360;
-	while (i != 460)
-		my_mlx_pixel_put(&img, 536, i++, 0xFFFFFF);
-    mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-    mlx_loop(mlx);
-	*/
 }
